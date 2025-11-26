a48bec5d88cfab334f0630802282175c189a3713
Daniel-M-Sena <danielmartinsdesena@gmail.com>
2025-11-25 23:57:06 -0300

    Update GiftList.tsx

--- src/components/GiftList.tsx
+++ src/components/GiftList.tsx
@@ -230,7 +230,7 @@ export default function GiftList({
           </div>
         ) : (
           <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8">
               {availableGifts.map((gift) => (
                 <button
                   key={gift.id}
