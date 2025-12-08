0967846771cecfc5ec63ae67d147613df25104cf
Daniel-M-Sena <danielmartinsdesena@gmail.com>
2025-12-08 20:57:29 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
   }, []);
 
   return (
-    <div className="relative h-screen mt-16">
+    <div className="relative h-[60vh] md:h-screen mt-16">
       <div className="relative h-full overflow-hidden">
         {carouselImages.map((image, index) => (
           <div
